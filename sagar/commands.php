def6b2016c9c1260ec66e206d9003898d39ff80f
Sagar Sejawar <Sagar Sejawar>
2016-03-21 13:35:18 +0530

    Commiting my sagar directory

---
+++ sagar/commands.php
@@ -0,0 +1,8 @@
+Hi, This file will hold the important commands as well as documentation related to any command:
+
+1) touch file-name : Used to create a file into your current working directory.
+2) vi file-name OR vim file-name : Used to write into file.
+
+Note : After writting to the file, you can use ESC :wq to write the file into disc and press ENTER to quit VIM.
+
+3) 
